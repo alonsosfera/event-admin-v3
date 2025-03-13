06794b5adcd7bcfdd2bfaf91bb42603ebf69be03
Arturo <turriscontreras@gmail.com>
2025-03-13 13:16:59 -0600

    Se cambio moment por dayjs

--- pages/api/auth/password-change.js
+++ pages/api/auth/password-change.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt"
-import moment from "moment"
+import dayjs from "dayjs"
 import { prisma } from "../../../lib/prisma"
 
 export default async function handler(req, res) {
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
 
       if (!userRecovery) {
         return res.status(404).json({ message: "No se encontró la solicitud de recuperación" })
-      } else if (moment(userRecovery.createdAt).add(1, "day").isBefore(moment())) {
+      } else if (dayjs(userRecovery.createdAt).add(1, "day").isBefore(dayjs())) {
         await prisma.passwordRecovery.delete({ where: { id: recovery } })
         return res.status(400).json({ expired: true, message: "La solicitud de recuperación ha expirado, favor de solicitar una nueva." })
       }
