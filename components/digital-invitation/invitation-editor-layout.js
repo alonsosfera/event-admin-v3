7ef7e7ef3a507104fb90503172b6b3bddde0febe
Arturo <turriscontreras@gmail.com>
2025-03-31 12:07:14 -0600

    Se dividió 1 componente en varios componentes.

---
+++ components/digital-invitation/invitation-editor-layout.js
@@ -0,0 +1,72 @@
+import { Col, Row } from "antd"
+import { EditableFieldsList } from "./editable-fields-list"
+import { InvitationConfigItem } from "../designs/invitations/invitation-config-item"
+import { InvitationConfigMapHost } from "./invitation-config-map-host"
+
+export const InvitationEditorLayout = ({
+  updatedCoordinates,
+  newItems,
+  state,
+  customConfig,
+  onValueChange,
+  onLinkChange,
+  handleAddItem,
+  scaleFactor,
+  selectedFile,
+  selectedInvitationUrl,
+  previewFile,
+  activeSource,
+  event,
+  onScaleFactorChange,
+  onPositionChange,
+  onDeleteItem
+}) => {
+  const finalCoordinates = newItems.length > 0 ? newItems : updatedCoordinates
+
+  return (
+    <Row gutter={24}>
+      {updatedCoordinates.length > 0 ? (
+        <EditableFieldsList
+          coordinates={updatedCoordinates}
+          state={state}
+          customConfig={customConfig}
+          onValueChange={onValueChange}
+          onLinkChange={onLinkChange}
+        />
+      ) : (
+        <Col span={8}>
+          <InvitationConfigItem
+            onSubmit={handleAddItem}
+            scaleFactor={scaleFactor}
+            selectedFile={selectedFile}
+          />
+        </Col>
+      )}
+
+      <Col span={16}>
+        <InvitationConfigMapHost
+          selectedInvitationUrl={
+            activeSource === "upload"
+              ? previewFile?.previewUrl
+              : activeSource === "select"
+              ? selectedInvitationUrl
+              : event?.digitalInvitation?.fileUrl
+          }
+          onScaleFactorChange={onScaleFactorChange}
+          event={{
+            ...event,
+            digitalInvitation: {
+              ...event.digitalInvitation,
+              canvaMap: {
+                ...event.digitalInvitation?.canvaMap,
+                coordinates: finalCoordinates
+              }
+            }
+          }}
+          onDeleteItem={onDeleteItem}
+          onPositionChange={onPositionChange}
+        />
+      </Col>
+    </Row>
+  )
+}
