d3eb5e78488f8886d7b0302fb27e924fb93e5047
Arturo <turriscontreras@gmail.com>
2025-05-07 14:18:57 -0600

    [EAV-65] Se cambió la forma de hacer el get, en vez de llamar al premiumInvitationId se llama al invitationId para recibir también los datos de cada invitado.

---
+++ pages/api/premium-invitation/i/[invitationId].js
@@ -0,0 +1,48 @@
+import { prisma } from '@/lib/prisma';
+import short from "short-uuid";
+
+export default async function handler(req, res) {
+  const { invitationId } = req.query;
+  console.log("invitationId", invitationId);
+  
+  const cleanedId = invitationId.substring(2);
+  console.log("cleanedId ", cleanedId);
+  
+  if (!cleanedId) {
+    console.error("Missing invitationId");
+    return res.status(400).json({ message: "Missing invitationId" });
+  }
+
+  try {
+    const translator = short();
+    const long = translator.toUUID(cleanedId)
+    console.log("long: ", long);
+    
+    const invitation = await prisma.invitation.findUnique({
+      where: { id: translator.toUUID(cleanedId) },
+      include: {
+        event: { 
+          include: {
+            premiumInvitation: {
+              include: {
+                sections: {
+                  orderBy: { order: 'asc' }
+                }
+              }
+            }
+          }
+        }
+      },
+    });
+
+    if (!invitation) {
+      console.error("No invitation found for this ID");
+      return res.status(404).json({ message: "No invitation found for this ID" });
+    }
+
+    res.status(200).json(invitation);
+  } catch (error) {
+    console.error("Error fetching invitation:", error);
+    res.status(500).json({ message: "Error fetching invitation", error });
+  }
+}
